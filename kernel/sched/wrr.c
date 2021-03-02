f5ba724ad2ba45331f3adf6d545a5faab993081b
RohanKumarSachdeva <rohan231094@gmail.com>
2021-03-02 07:47:16 -0800

    modified .next value of rt.c to point to sched_wrr_class and .next value of wrr.c to point to fair_sched_class

--- kernel/sched/wrr.c
+++ kernel/sched/wrr.c
@@ -4,30 +4,29 @@
  * All the scheduling class methods:
  */
 const struct sched_class sched_wrr_class = {
-        /* .next is NULL */
-        /* no enqueue/yield_task for idle tasks */
 
-        /* dequeue is not valid, we print a debug message there: */
-        .dequeue_task           = dequeue_task_idle,
+	.next			= &fair_sched_class,
+	.enqueue_task		= enqueue_task_wrr,
+        .dequeue_task           = dequeue_task_wrr,
 
-        .check_preempt_curr     = check_preempt_curr_idle,
+        .check_preempt_curr     = check_preempt_curr_wrr,
 
-        .pick_next_task         = pick_next_task_idle,
-        .put_prev_task          = put_prev_task_idle,
-        .set_next_task          = set_next_task_idle,
+        .pick_next_task         = pick_next_task_wrr,
+        .put_prev_task          = put_prev_task_wrr,
+        .set_next_task          = set_next_task_wrr,
 
 #ifdef CONFIG_SMP
-        .balance                = balance_idle,
-        .select_task_rq         = select_task_rq_idle,
+        .balance                = balance_wrr,
+        .select_task_rq         = select_task_rq_wrr,
         .set_cpus_allowed       = set_cpus_allowed_common,
 #endif
 
-        .task_tick              = task_tick_idle,
+        .task_tick              = task_tick_wrr,
 
-        .get_rr_interval        = get_rr_interval_idle,
+        .get_rr_interval        = get_rr_interval_wrr,
 
-        .prio_changed           = prio_changed_idle,
-        .switched_to            = switched_to_idle,
-        .update_curr            = update_curr_idle,
+        .prio_changed           = prio_changed_wrr,
+        .switched_to            = switched_to_wrr,
+        .update_curr            = update_curr_wrr,
 };
 
