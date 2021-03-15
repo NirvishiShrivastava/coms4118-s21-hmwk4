7a845f3e53c24cd1c8c34daed75fa792ad02bf75
Nirvishi Shrivastava <ns3499@columbia.edu>
2021-03-15 11:50:15 -0700

    Minor checkpatch fixes and adding timeslice checks

--- kernel/sched/wrr.c
+++ kernel/sched/wrr.c
@@ -24,7 +24,6 @@ void init_wrr_rq(struct wrr_rq *wrr_rq)
 static void __enqueue_wrr_entity(struct rq *rq, struct sched_wrr_entity *wrr_se, unsigned int flags)
 {
 	pr_info("Inside function: %s", __func__);
-	
 	struct wrr_rq *wrr_rq = &rq->wrr;
 	if (flags & ENQUEUE_HEAD)
 		list_add(&wrr_se->run_list, &rq->wrr.wrr_rq_list);
@@ -39,32 +38,25 @@ static void enqueue_task_wrr(struct rq *rq, struct task_struct *p, int flags)
 	pr_info("Inside function: %s", __func__);
 
 	struct sched_wrr_entity *wrr_se = &p->wrr;
-	
 	if (flags & ENQUEUE_WAKEUP)
 		wrr_se->timeout = 0;
-	
 	__enqueue_wrr_entity(rq, wrr_se, flags);
 	add_nr_running(rq, 1);
 
 }
 
-/* 
- *The function keeps track of the time the current process spent 
- *executing on the CPU and updates the current task's runtime 
- *statistics.
+/* The function keeps track of the time the current process spent 
+ * executing on the CPU and updates the current task's runtime statistics.
  */
 static void update_curr_wrr(struct rq *rq)
 {
 	pr_info("Inside function: %s", __func__);
-	
 	struct task_struct *curr = rq->curr;
 	struct sched_wrr_entity *wrr_se = &curr->wrr;
 	u64 delta_exec;
 	u64 now;
-
 	if (curr->sched_class != &wrr_sched_class)
 		return;
-
 	now = rq_clock_task(rq);
 	
 	/* delta_exec shows the time that current process ran */
@@ -83,13 +75,12 @@ static void update_curr_wrr(struct rq *rq)
 	/*  update exec_start time for next time */
 	curr->se.exec_start = now;
 	cgroup_account_cputime(curr, delta_exec);
-
 }
 
-static void __dequeue_wrr_entity(struct rq *rq, struct sched_wrr_entity *wrr_se, unsigned int flags)
+static void __dequeue_wrr_entity(struct rq *rq, 
+		struct sched_wrr_entity *wrr_se, unsigned int flags)
 {
 	pr_info("Inside function: %s", __func__);
-	
 	list_del_init(&wrr_se->run_list);
 	rq->wrr.total_rq_weight -= wrr_se->wrr_se_weight;
 	--rq->wrr.wrr_nr_running;
@@ -101,7 +92,6 @@ static void dequeue_task_wrr(struct rq *rq, struct task_struct *p, int flags)
 	pr_info("Inside function: %s", __func__);
 	
 	struct sched_wrr_entity *wrr_se = &p->wrr;
-
 	update_curr_wrr(rq);
 	__dequeue_wrr_entity(rq, wrr_se, flags);
 	sub_nr_running(rq, 1);
@@ -175,22 +165,23 @@ static void task_tick_wrr(struct rq *rq, struct task_struct *p, int queued)
 
 	update_curr_wrr(rq);
 	
-	/* TODO: Check if we require this function
-	* update_wrr_rq_load_avg(rq_clock_pelt(rq), rq, 1);
-	*/
-	
 	if (p->policy != SCHED_WRR)
 		return;
+	pr_info("*************Before timeslice decrement************");
 
-	if (--p->wrr.wrr_se_timeslice)
+	if (--p->wrr.wrr_se_timeslice){
+		pr_info("++++Timeslice of process : %d++++++", p->wrr.wrr_se_timeslice);
 		return;
+	}
 
 	wrr_se->wrr_se_timeslice = DEFAULT_WRR_TIMESLICE * wrr_se->wrr_se_weight;
+	pr_info("++++++++++Timeslice of process after resetting: %d++++++", wrr_se->wrr_se_timeslice);
 
 	/* Requeue to the end of queue */
 	if (wrr_se->run_list.prev != wrr_se->run_list.next) {
 		requeue_task_wrr(rq, p, 0);
 		resched_curr(rq);
+		pr_info("========Inside resched if condition ========");
 	}
 }
 
@@ -243,11 +234,10 @@ select_task_rq_wrr(struct task_struct *p, int cpu, int sd_flag, int flags)
 	unsigned long min_weight, temp_weight;
 	struct rq *rq;
 	int cpu_iter;
-
+	
 	min_weight = LONG_MAX;
-
 	rcu_read_lock();
-
+	
 	for_each_cpu(cpu_iter, p->cpus_ptr) {
 		pr_info("Iterating over CPU %d", cpu_iter);
 		rq = cpu_rq(cpu_iter);
@@ -284,26 +274,22 @@ const struct sched_class wrr_sched_class = {
 
 	.next			= &fair_sched_class,
 	.enqueue_task		= enqueue_task_wrr,
-        .dequeue_task           = dequeue_task_wrr,
+	.dequeue_task           = dequeue_task_wrr,
 	.yield_task		= yield_task_wrr,
 	.check_preempt_curr     = check_preempt_curr_wrr,
-
-        .pick_next_task         = pick_next_task_wrr,
-        .put_prev_task          = put_prev_task_wrr,
-        .set_next_task          = set_next_task_wrr,
+	.pick_next_task         = pick_next_task_wrr,
+	.put_prev_task          = put_prev_task_wrr,
+	.set_next_task          = set_next_task_wrr,
 
 #ifdef CONFIG_SMP
-        .balance                = balance_wrr,
-        .select_task_rq         = select_task_rq_wrr,
-        .set_cpus_allowed       = set_cpus_allowed_common,
+	.balance                = balance_wrr,
+	.select_task_rq         = select_task_rq_wrr,
+	.set_cpus_allowed       = set_cpus_allowed_common,
 #endif
-
-        .task_tick              = task_tick_wrr,
-
-        .get_rr_interval        = get_rr_interval_wrr,
-
-        .prio_changed           = prio_changed_wrr,
-        .switched_to            = switched_to_wrr,
-        .update_curr            = update_curr_wrr,
+	.task_tick              = task_tick_wrr,
+	.get_rr_interval        = get_rr_interval_wrr,
+	.prio_changed           = prio_changed_wrr,
+	.switched_to            = switched_to_wrr,
+	.update_curr            = update_curr_wrr,
 };
 
