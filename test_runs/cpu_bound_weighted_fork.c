b4120e5fa768d6a442c6373d05aa6e7b139dacfd
Rishabh Gupta <rg3334@columbia.edu>
2021-03-18 16:12:51 -0700

    added explanation for idle balance check

---
+++ test_runs/cpu_bound_weighted_fork.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#define __NR_get_wrr_info 436
+#define __NR_set_wrr_weight 437
+
+int wrr_set(int weight)
+{
+        return syscall(__NR_set_wrr_weight, weight);
+}
+
+int main()
+{
+        int ret;
+        printf("Entering fork call \n");
+
+        if(fork()) {
+                ret = wrr_set(4);
+                if (ret < 0) {
+                        fprintf(stderr, "error from wrr_set system call: %s\n",
+                                strerror(errno));
+                        exit(EXIT_FAILURE);
+                }
+                while(1) {}
+        } else {
+                if(fork()) {
+                        ret = wrr_set(20);
+                        if (ret < 0) {
+                                fprintf(stderr, "error from wrr_set system call: %s\n",
+                                        strerror(errno));
+                                exit(EXIT_FAILURE);
+                        }
+			while (1) {}
+                } else {
+                        ret = wrr_set(100);
+                        if (ret < 0) {
+                                fprintf(stderr, "error from wrr_set system call: %s\n",
+                                        strerror(errno));
+                                exit(EXIT_FAILURE);
+                        }
+			while (1) {}
+		} 
+        }
+
+        return 0;
+}
+
